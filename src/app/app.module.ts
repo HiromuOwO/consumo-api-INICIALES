6b4c7d8d3f2a4f2850dbc75e5c9c7800ebd79b65
HiromuOwO <edithperezdejesus00@gmail.com>
2024-11-15 21:23:23 -0600

    Tabla sin formato

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-
+import { UserService } from './services/user.service';
+import { UserListComponent } from './components/user-list/user-list.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+declarations: [
+AppComponent,
+UserListComponent
+],
+imports: [
+BrowserModule,
+HttpClientModule
+],
+providers: [UserService],
+bootstrap: [AppComponent]
 })
 export class AppModule { }
